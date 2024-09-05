7dc5aa13ce98ce688b4741ac031ba52d13192602
zenjin <zenjin@student.unimelb.edu.au>
2024-09-05 17:41:22 +1000

    update accounts

--- src/pages/viewaccount.tsx
+++ src/pages/viewaccount.tsx
@@ -11,11 +11,29 @@ export const Viewaccount = () => {
       image: "/assets/section1.png",
       balance: "$100.00",
     },
+    {
+      name: "Saving",
+      bsb: "010-010",
+      accNo: "1234 5678",
+      image: "/assets/section2.png",
+      balance: "$1000.00",
+    },
   ];
   return (
     <Container>
-      <div className="flex w-[60%]">
-        <ModalAccounts accounts={accounts} />
+      <div>
+        <div className="flex flex-row">
+          <div className="flex w-[60%]">
+            <ModalAccounts accounts={accounts} />
+          </div>
+          <div className="flex flex-[1]">
+            <img
+              src="assets/old-person.png"
+              alt="person-interact"
+              className="w-[347px] pt-space-6"
+            />
+          </div>
+        </div>
       </div>
     </Container>
   );
