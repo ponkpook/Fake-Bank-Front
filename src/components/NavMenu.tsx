ae49fcb53b79dbc2894ebae6138e91e5176374e9
Estella <estellaxu127@gmail.com>
2024-09-15 23:52:00 +1000

    Create NavMenu.tsx

---
+++ src/components/NavMenu.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import NavItem from "./NavItem";
+
+interface NavMenuProps {
+  isMenuOpen: boolean;
+  isLoggedIn: boolean;
+}
+
+const NavMenu: React.FC<NavMenuProps> = ({ isMenuOpen, isLoggedIn }) => {
+  return (
+    <div
+      className={`md:hidden ${isMenuOpen ? "block" : "hidden"} nav-bg-green shadow-lg`}
+    >
+      <div className="flex flex-col p-4 space-y-2">
+        <NavItem isLoggedIn={isLoggedIn} />
+        {!isLoggedIn ? (
+          <>
+            <Link
+              to="/login"
+              className="block text-black rounded-full font-light px-space-4 py-space-2 shadow-lg hover:bg-native-milk/50 transition"
+            >
+              Log In
+            </Link>
+            <Link
+              to="/signup"
+              className="block bg-native-red rounded-full text-black font-light px-space-4 py-space-2 shadow-lg hover:bg-orange-600 transition"
+            >
+              Sign Up
+            </Link>
+          </>
+        ) : (
+          <Link
+            to="/homepage"
+            className="block bg-native-red rounded-full text-black font-light px-space-4 py-space-2 shadow-lg hover:bg-orange-600 transition"
+          >
+            Sign Off
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default NavMenu;
+
+
