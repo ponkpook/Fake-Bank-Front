75e31fe3df6c0c1073db55c68d1e423722f88296
xrightzzZ <ezjiang@student.unimelb.edu.au>
2024-10-25 01:25:16 +1100

    fix: set username to null after sign off

--- src/components/SignOffButton.tsx
+++ src/components/SignOffButton.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const SignOffButton: React.FC = () => {
-    return (
-        <div className="hidden md:flex justify-end mr-20">
-            <Link
-                to="/"
-                className="bg-native-red rounded-full text-black font-medium font-['Poppins'] px-space-4 py-space-2 shadow-lg hover:bg-orange-600 transition"
-            >
-                Sign Off
-            </Link>
-        </div>
-    );
+  const handleSignOff = () => {
+    console.log('Signing off');
+    sessionStorage.removeItem('username');
+  };
+  return (
+    <div className="hidden md:flex justify-end mr-20">
+      <Link
+          to="/"
+          className="bg-native-red rounded-full text-black font-medium font-['Poppins'] px-space-4 py-space-2 shadow-lg hover:bg-orange-600 transition"
+          onClick={handleSignOff}
+      >
+          Sign Off
+      </Link>
+    </div>
+  );
 };
 
 export default SignOffButton;
